020dc31c9fca83690c3ce03e3ec5368564659ca6
maximar <cristinamar2000@gmail.com>
2024-12-10 16:28:25 -0600

    Update Database.swift
    Add documentation

--- ManagerApp/VIewModels/Database.swift
+++ ManagerApp/VIewModels/Database.swift
@@ -6,16 +6,19 @@
 //
 
 // Database.swift
+// Only handles the storage logic of the User object
 
 import Foundation
 
 class Database {
     static var users: [User] = []
 
+    // Allows you to save a user
     static func saveUser(user: User) {
         users.append(user)
     }
 
+    //Allows you to search for a user
     static func getUserByEmail(email: String) -> User? {
         return users.first { $0.email == email }
     }
